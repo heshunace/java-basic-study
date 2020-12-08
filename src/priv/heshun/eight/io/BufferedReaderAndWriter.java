747480ae1916edb6d47ce7b8ed9580c0e3772a16
heshun <heshunace@gmail.com>
2020-12-08 11:19:01 +0800

    缓冲字节流实现文本文件的读取和写出

--- src/priv/heshun/eight/io/BufferedReaderAndWriter.java
+++ src/priv/heshun/eight/io/BufferedReaderAndWriter.java
@@ -3,53 +3,72 @@ package priv.heshun.eight.io;
 import java.io.*;
 
 /**
- * 缓冲字符流实现非文本文件的读取和写出
+ * 缓冲字节流实现文本文件的读取和写出
  *
  * @author heshun
- * @create 2020-12-07-16:34
+ * @create 2020-12-08-10:56
  **/
 public class BufferedReaderAndWriter {
 
     public static void main(String[] args) {
+        /*
+            从文件读取
+         */
         // 1.创建文件
-        File file = new File("C:\\Users\\heshu\\Desktop\\夺金营救.mkv");
-        File file2 = new File("C:\\Users\\heshu\\Desktop\\夺金营救2.mkv");
-        // 2.创建节点流
-        FileInputStream fis = null;
-        FileOutputStream fos = null;
+        File file = new File("C:\\Users\\heshu\\Desktop\\1.txt");
+        File file2 = new File("C:\\Users\\heshu\\Desktop\\2.txt");
+        // 2.创建流
+        FileReader fileReader = null;
+        FileWriter fileWriter = null;
         try {
-            fis = new FileInputStream(file);
-            fos = new FileOutputStream(file2);
             // 3.创建处理流，并套接在节点流上
-            BufferedInputStream bis = new BufferedInputStream(fis);
-            BufferedOutputStream bos = new BufferedOutputStream(fos);
-            // byte数组读入
-            int len;
-            byte[] data = new byte[1024];
-            // 读入并写出
-            System.out.println("开始读取和写出");
-            long start = System.currentTimeMillis();
-            while((len = bis.read(data)) != -1) {
-                bos.write(data, 0 ,len);
+            fileReader = new FileReader(file);
+            fileWriter = new FileWriter(file2);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            // 4.读入数据
+
+            // 方式一：单个char读入
+            int data;
+            while((data = bufferedReader.read()) != -1) {
+                bufferedWriter.write(data);
+                // 若未达到缓存的大小，需要手动调用flush
+                // 若达到缓存的大小，则自动调用flush
+                bufferedWriter.flush();
             }
-            long end = System.currentTimeMillis();
-            System.out.println("读取和写出完成，耗时：" + (end - start));
-            // 测试结果：1.64GB mkv文件，耗时14202
-        } catch (Exception e) {
+            System.out.println("读取和写出完成");
+
+            // 方式二：char数组读入
+            int len;
+            char[] data2 = new char[5];
+            while((len = bufferedReader.read(data2)) != -1) {
+                bufferedWriter.write(data2, 0 , len);
+                bufferedWriter.flush();
+            }
+
+            // 方式三：整行读入
+            String data3;
+            while((data3 = bufferedReader.readLine()) != null) {
+                bufferedWriter.write(data3 + "\n");
+                bufferedWriter.flush();
+            }
+            System.out.println("读取和写出完成");
+
+        } catch(Exception e) {
             e.printStackTrace();
         } finally {
-            // 4.关闭流
-            // 关闭外层流的同时，内层流也会自动进行关闭；故内层流的关闭可以省略
-            if(fis != null) {
+            if(fileReader != null) {
+                // 4.流的关闭
                 try {
-                    fis.close();
+                    fileReader.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
-            if(fos != null) {
+            if(fileWriter != null) {
                 try {
-                    fos.close();
+                    fileWriter.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
